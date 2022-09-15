a58e865ccf4dea533b220fcb0c40a670626f5c7b
Marco Flores <hfmarcoalexander@hotmail.com>
2022-09-15 00:10:58 -0500

    Solving

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-class Registro {
-    base(nombre,autor,fecha,paginas,idioma,tipoarchivo){
-        this.nombre = nombre.toUpperCase();
+class registro {
+    constructor(nombre, autor, fecha, paginas, idioma, tipoarchivo){
+        this.nombre = nombre;
         this.autor = autor;
         this.fecha = fecha;
         this.paginas = paginas;
@@ -8,29 +8,29 @@ class Registro {
         this.tipoarchivo = tipoarchivo;
     }
 }
+
 function ingresardocumento() {
     let libros = [];
     let numeroentradas = parseInt(prompt("¿Cuántos documentos desea almacenar?"));
-    for (let i = 0; i < numeroentradas; i++){
-    let nombre = prompt("Ingrese el título");
-    let autor = prompt("Ingrese el autor");
-    let fecha = prompt("sds")
-    let paginas = parseInt(prompt("Ingrese el número de páginas"));
-    let idioma = prompt("El documento en qué idioma se encuentra");
-    let tipoarchivo = prompt("Tipo de documento(pdf/doc/epub)");
-    let nuevoregistro = new Registro(nombre,autor,fecha,paginas,idioma,tipoarchivo);
-    libros.push(nuevoregistro)
+    for (let i = 0; i < numeroentradas; i++) {
+        let nombre = prompt("Ingrese el título");
+        let autor = prompt("Ingrese el autor");
+        let fecha = prompt("Ingresar la fecha");
+        let paginas = prompt("Ingrese el número de páginas");
+        let idioma = prompt("El documento en qué idioma se encuentra");
+        let tipoarchivo = prompt("Tipo de documento(pdf/doc/epub)");
+        let nuevoregistro = new registro(nombre,autor,fecha,paginas,idioma,tipoarchivo)
+        libros.push(nuevoregistro);
     }
+
     return libros
 }
-function registrodata(libros){
-    for(let libro of libros){
-        console.log(libro)
-    }
-}
-function main(){
+
+
+
+function main() {
     let libros = ingresardocumento()
-    registrodata(libros)
+    console.log(libros)
 }
 
 main()
